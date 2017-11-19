7a0e855a9ed601fcf47b478285a8d4cdd98ece1d
VladDrabynka <vlad.un1que.96@gmail.com>
2017-11-19 22:49:09 +0200

    Exception handler (@ControllerAdvice), POJO for exceptions are added.

---
+++ src/main/java/com/netcracker/model/ApiError.java
@@ -0,0 +1,54 @@
+package com.netcracker.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by V.Drabynka on 16.11.2017.
+ */
+
+public class ApiError {
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    private ApiError() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        this();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ApiError(HttpStatus status, String message, String error) {
+        this();
+        this.status = status;
+        this.message = message;
+        errors = Arrays.asList(error);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
