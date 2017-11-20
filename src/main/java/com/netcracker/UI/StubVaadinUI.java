9ede337444c985ca89fcbc47a1115e01425f7784
omelchenko <dok.ttv@gmail.com>
2017-11-20 16:24:12 +0200

    UI forms (фиксы близко)

--- src/main/java/com/netcracker/UI/StubVaadinUI.java
+++ src/main/java/com/netcracker/UI/StubVaadinUI.java
@@ -3,13 +3,18 @@ package com.netcracker.UI;
 import com.netcracker.model.StubUser;
 import com.vaadin.annotations.Theme;
 import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.Position;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Odyniuk on 06/11/2017.
@@ -27,15 +32,21 @@ public class StubVaadinUI extends UI {
 
     @Autowired
     public StubVaadinUI(StubUserEditor stubUserEditor) {
-        this.stubUserEditor= stubUserEditor;
+        this.stubUserEditor = stubUserEditor;
         this.grid = new Grid<>(StubUser.class);
         this.addNewBtn = new Button("New user", VaadinIcons.PLUS);
     }
 
     @Override
     protected void init(VaadinRequest request) {
-        HorizontalLayout actions = new HorizontalLayout(addNewBtn);
-        VerticalLayout mainLayout = new VerticalLayout(actions, grid, stubUserEditor);
+
+        VerticalLayout mainLayout = new VerticalLayout();
+        HorizontalLayout primaryAreaLayout = new HorizontalLayout();
+        VerticalLayout addUsersLayout = new VerticalLayout();
+        addUsersLayout.addComponentsAndExpand(addNewBtn, grid, stubUserEditor);
+        addUsersLayout.getComponent(0).setHeight("10%");
+        primaryAreaLayout.addComponentsAndExpand(initLeftPanel(), addUsersLayout);
+        mainLayout.addComponentsAndExpand(initTopPanel(), primaryAreaLayout);
         setContent(mainLayout);
 
         grid.setHeight(300, Unit.PIXELS);
@@ -57,4 +68,60 @@ public class StubVaadinUI extends UI {
         grid.setItems(users);
     }
 
+    private HorizontalLayout initTopPanel() {
+        HorizontalLayout topPanelLayout = new HorizontalLayout();
+        topPanelLayout.setWidth("100%");
+        topPanelLayout.setSpacing(false);
+        HashMap<String, VaadinIcons> buttonsMap = new HashMap<>(5);
+        buttonsMap.put("Main page", VaadinIcons.REFRESH);
+        buttonsMap.put("News", VaadinIcons.DOCTOR);
+        buttonsMap.put("Users", VaadinIcons.ARCHIVE);
+        buttonsMap.put("Pets", VaadinIcons.HOSPITAL);
+        buttonsMap.put("Bulletin board", VaadinIcons.YOUTUBE);
+        for (Map.Entry<String, VaadinIcons> entry : buttonsMap.entrySet()) {
+            Button button = new Button(entry.getKey(), entry.getValue());
+            button.setWidth("100%");
+            if (entry.getKey().equals("Main page")) {
+                button.addStyleName(ValoTheme.BUTTON_DANGER);
+            } else {
+                button.addStyleName(ValoTheme.BUTTON_HUGE);
+            }
+            topPanelLayout.addComponentsAndExpand(button);
+            button.addClickListener(clickEvent -> {
+                Notification notification = new Notification("No " + entry.getKey() + " yet!!!",
+                        Notification.Type.HUMANIZED_MESSAGE);
+                notification.setPosition(Position.MIDDLE_CENTER);
+                notification.show(Page.getCurrent());
+            });
+        }
+        return topPanelLayout;
+    }
+
+    private VerticalLayout initLeftPanel() {
+        VerticalLayout leftPanelLayout = new VerticalLayout();
+        leftPanelLayout.setWidth("20%");
+        leftPanelLayout.setSpacing(false);
+
+        HashMap<String, VaadinIcons> buttonsMap = new HashMap<>(6);
+        buttonsMap.put("Profile", VaadinIcons.ABACUS);
+        buttonsMap.put("Pets", VaadinIcons.BOMB);
+        buttonsMap.put("Friends", VaadinIcons.BAN);
+        buttonsMap.put("Photos", VaadinIcons.PENCIL);
+        buttonsMap.put("Adverts", VaadinIcons.SPOON);
+        buttonsMap.put("Settings", VaadinIcons.MOON);
+        for (Map.Entry<String, VaadinIcons> entry : buttonsMap.entrySet()) {
+            Button button = new Button(entry.getKey(), entry.getValue());
+            button.setWidth("100%");
+            button.addStyleName(ValoTheme.BUTTON_QUIET);
+            leftPanelLayout.addComponentsAndExpand(button);
+            button.addClickListener(clickEvent -> {
+                Notification notification = new Notification("No " + entry.getKey() + " yet!!!",
+                        Notification.Type.HUMANIZED_MESSAGE);
+                notification.setPosition(Position.MIDDLE_CENTER);
+                notification.show(Page.getCurrent());
+            });
+        }
+
+        return leftPanelLayout;
+    }
 }
