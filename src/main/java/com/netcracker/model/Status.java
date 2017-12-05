c3ca0a0048d6bc0f73ea9b3b22931f04e97b2484
Simpson4eg <dok.ttv@gmail.com>
2017-12-05 03:15:25 +0200

    Status fix, testDB deleted

--- src/main/java/com/netcracker/model/Status.java
+++ src/main/java/com/netcracker/model/Status.java
@@ -9,6 +9,8 @@ import com.netcracker.model.group.Group;
 import com.netcracker.model.group.GroupConstant;
 import com.netcracker.model.pet.Pet;
 import com.netcracker.model.pet.PetConstant;
+import com.netcracker.model.request.FriendRequest;
+import com.netcracker.model.request.FriendRequestConstant;
 import com.netcracker.model.user.Profile;
 import com.netcracker.model.user.UsersProfileConstant;
 
@@ -27,6 +29,9 @@ public class Status extends BaseEntity {
     private List<Advertisement> advertisements;
     @Reference(PetConstant.PET_STATE)
     private List<Pet> pets;
+    @Reference(FriendRequestConstant.REQ_STATUS)
+    private List<FriendRequest> requests;
+
 
     public Status() {
     }
@@ -79,6 +84,14 @@ public class Status extends BaseEntity {
         this.pets = pets;
     }
 
+    public List<FriendRequest> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(List<FriendRequest> requests) {
+        this.requests = requests;
+    }
+
     @Override
     public String toString() {
         return "Status{" +
@@ -87,6 +100,7 @@ public class Status extends BaseEntity {
                 ", profiles=" + profiles +
                 ", advertisements=" + advertisements +
                 ", pets=" + pets +
+                ", requests=" + requests +
                 '}';
     }
 }
