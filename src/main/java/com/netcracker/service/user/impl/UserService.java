b1374d1c8b626cefa7d3709752a7e8512d91ae21
Lux <levil13@mail.ru>
2017-12-18 19:07:01 +0200

    Added logout button

--- src/main/java/com/netcracker/service/user/impl/UserService.java
+++ src/main/java/com/netcracker/service/user/impl/UserService.java
@@ -1,9 +1,14 @@
 package com.netcracker.service.user.impl;
 
 import com.netcracker.model.user.User;
+import com.netcracker.ui.login.LoginPage;
+import com.vaadin.ui.Notification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class UserService {
@@ -12,6 +17,7 @@ public class UserService {
     UserDetailsServiceImpl userDetailsService;
 
     public User getCurrentUser() {
+        SecurityContext securityContextHolder = SecurityContextHolder.getContext();
         String login = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return userDetailsService.loadUserByUsername(login);
     }
